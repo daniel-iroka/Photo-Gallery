38ba0560cf46be02935ff74f8eef0ff2ec377370
daniel-iroka <danieliroka67@gmail.com>
2022-01-28 18:26:49 +0100

    Implemented and created and instance of our API using retrofit

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class PhotoGalleryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_gallery)
@@ -15,5 +16,6 @@ class PhotoGalleryActivity : AppCompatActivity() {
                 .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
                 .commit()
         }
+
     }
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,16 +1,56 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
 
+
+    // Todo - When I come back, I will go through Executing a web request again and then move on.........
+
     private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val retrofit : Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create()) // this expects a converter factory which creates an instance of scalarConverters that will be used by retrofit
+            .build()
+
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+
+        val flickrApiHomePageRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrApiHomePageRequest.enqueue(object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                Log.d(TAG,"Response received: ${response.body()}")
+            }
+
+        })
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
