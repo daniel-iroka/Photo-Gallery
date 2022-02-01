7b6fcf901c4ab51b9a2e048cf36df71a39123710
daniel-iroka <danieliroka67@gmail.com>
2022-02-02 00:14:14 +0100

    little correction

--- app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.photogallery
 
-// This is our Model class where we will store all our JSON data which includes information about a photo
+// This is our Model class where we will stash all our recieved JSON data(recent interesting photos)
+// This class will hold information for any photo such as its title, id, url and the rest.
 
 data class GalleryItem(
     var title: String = "",
