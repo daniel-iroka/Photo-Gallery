80b6cf8d0f3cbf46268676e6d94033cd68a6db9c
daniel-iroka <danieliroka67@gmail.com>
2022-02-08 17:39:57 +0100

    No need for this anymore

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.bignerdranch.android.photogallery.api
-
-// This class maps to the outermost object("photos, stat") in the JSON hierarchy
-// In simple terms, it maps to everything in the JSON data
-
-class FlickrResponse {
-    lateinit var photos: PhotoResponse
-}
